729fbc3dc951d33d403fc4f06530d14fac295e5f
bukrekdeniz <110018388+bukrekdeniz@users.noreply.github.com>
2022-07-26 12:36:25 +0300

    PayByBank.configure to example main

--- app/src/main/java/com/ecospend/paylinkmobilesdk/app/PaylinkMobileSDK.kt
+++ app/src/main/java/com/ecospend/paylinkmobilesdk/app/PaylinkMobileSDK.kt
@@ -8,8 +8,8 @@ class PaylinkMobileSDK : Application() {
     override fun onCreate() {
         super.onCreate()
         PayByBank.configure(
-            clientId = "910162c0-a0e6-40b8-b66d-f6a9d56bee0f",
-            clientSecret = "e5cfcc4b22b7ef8059b53bb54c9dca15f28de13bd90b4567a679523ab86cd8d0",
+            clientId = "*******************",
+            clientSecret = "******************",
             environment = PayByBankEnvironment.Sandbox
         )
     }
